94127485029427c5509ed5bdd2d727751ed91efa
Sara Mazal <32996161+saramazal@users.noreply.github.com>
2022-06-07 16:27:36 +0300

    Update posts.js

--- controllers/posts.js
+++ controllers/posts.js
@@ -7,9 +7,9 @@ const router = express.Router();
 
 export const getPosts = async (req, res) => { 
     try {
-        const postMessages = await PostMessage.find();
+        const PostMessages = await PostMessage.find();
                 
-        res.status(200).json(postMessages);
+        res.status(200).json(PostMessages);
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
